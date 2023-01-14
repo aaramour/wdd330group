9cbcae7507b874e124a3731f5a008fa5db2955de
antonioyed <yed21001@byui.edu>
2023-01-14 12:39:23 -0500

    new test

--- src/js/product.js
+++ src/js/product.js
@@ -3,13 +3,13 @@ import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addToCart(product) {
+function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addToCart(product);
+  addProductToCart(product);
 }
 
 // add listener to Add to Cart button
