0bf30011895642bd41fc60f24e09f94aff7af43a
Sterling Welker <sterlingjdub@gmail.com>
2023-01-14 18:08:01 -0700

    The add to cart function appears to be working

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,9 +2,10 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const htmlItems = cartItemTemplate(cartItems);
+  document.querySelector(".product-list").innerHTML = htmlItems;
 }
+
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
