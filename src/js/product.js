693aa9dbbd12b1381200ab7cbd2f40f5900b0299
aaramour <aaramour@byui.edu>
2023-01-20 21:10:57 -0500

    Completed the team activity successfully

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from './ProductDetails.mjs';
 import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
 const productId = getParam('product');
-console.log(productId);
+// console.log(productId);
 
-console.log(dataSource.findProductById(productId));
+// console.log(dataSource.findProductById(productId));
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
