ba4a459a61ff37a7a7b4f7ad88402ddcb805eb58
aaramour <aaramour@byui.edu>
2023-01-26 21:58:48 -0500

    finished team activity and stretch 01

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,7 +9,7 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    this.path = `../public/json/${this.category}.json`;
   }
   getData() {
     return fetch(this.path)
