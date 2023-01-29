b000c24bceb166f1117566c9a9cc2eead68e2663
aaramour <aaramour@byui.edu>
2023-01-28 20:40:51 -0500

    only show four active products

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,14 +2,17 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 export default class ProductList {
     constructor(category, dataSource, listElement) {
-        this.category = category;
+        // this.category = category;
         this.dataSource = dataSource;
+        // console.log(dataSource);
         this.listElement = listElement;
+        // console.log(listElement);
     }
+
     async init() {
-       
         const list = await this.dataSource.getData();
         this.renderList(list);
+        // console.log(list);
     } 
 
     renderList(list) {
@@ -18,7 +21,27 @@ export default class ProductList {
     }
 }
 
+// function isProductActive (product) {
+//     const activeProductIds = ["880RR", "985RF", "985PR", "344YJ"];
+//     if product.Id in activeProductIds (
+//         return true
+//     ) else (
+//         return false
+//     );
+// }
+
+function isProductActive (product) {
+    const activeProducts = ["880RR", "985RF", "985PR", "344YJ"];
+    const determiner = activeProducts.includes(product.Id);
+    // console.log(determiner);
+    return determiner;
+}
+
 function productCardTemplate(product) {
+    // console.log(product.Id);
+    // isProductActive(product);
+    let determiner = isProductActive(product);
+    if (determiner == true) {
     return `<li class="product-card">
     <a href="product_pages/index.html?product=${product.Id}">
     <img
@@ -29,4 +52,5 @@ function productCardTemplate(product) {
     <h2 class="card_name>${product.Name}</h2>
     <p class="product-card_price">$${product.FinalPrice}</p></a>
     </li>`;
+} else {}
 }
