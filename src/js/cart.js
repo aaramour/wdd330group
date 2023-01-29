c4aff16a481674ccd5684dd7f173d18e2a8bd0b9
aaramour <aaramour@byui.edu>
2023-01-28 21:58:32 -0500

    fixed empty cart error

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,12 +2,29 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
+  console.log(cartItems);
+  // const totalPrice = cartTotaler(cartItems); //compute and display the cart total
   const htmlItems = cartItemTemplate(cartItems);
   document.querySelector(".product-list").innerHTML = htmlItems;
 }
 
+function changeClass(thing, fromClassName, toClassName){ //create a function that takes a thing
+  thing.classList.remove(fromClassName); //and removes the class className from it
+  thing.classList.add(toClassName); // and adds another class
+};
+
+function cartTotaler(cartItems) { //function to compute and display the cart total
+  let totalPrice = 0;
+  const cartFooter = document.querySelector('.cart-footer');
+  if(cartItems) {
+    totalPrice += cartItems.FinalPrice;
+    changeClass(cartFooter, ".hidden", "")
+    return totalPrice;
+  } else {return}
+}
 
 function cartItemTemplate(item) {
+  if(item) { 
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
@@ -22,8 +39,14 @@ function cartItemTemplate(item) {
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-
+return newItem;
+ 
+} else {
+  const newItem = `<p class="cart-card">Your cart is currently empty.</p>`;
   return newItem;
+}
+
+
 }
 
 renderCartContents();
